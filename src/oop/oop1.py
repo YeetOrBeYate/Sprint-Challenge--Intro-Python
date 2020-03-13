16d8ff93d8c22e9f2df4e2088142ab782e13f143
Kyle Corbin <kylecorbin26@gmail.com>
2020-03-13 11:22:09 -0400

    Finnished the first oop file

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,41 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class
+
+class Vehicle:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+class FlightVehicle(Vehicle):
+    def __init__(self, name, color, size)
+    super().__init__(name, color)
+    self.size = size
+
+class Starship(FlightVehicle):
+    def __init__(self, name, color, size, engine)
+    super().__init__(name, color, size)
+    self.engine = engine
+
+class Airplane(FlightVehicle):
+    def __init__(self, name, color, size, wings)
+    super().__init__(name, color, size)
+    self.wings = wings
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name, color, tires)
+    super().__init__(name, color)
+    self.tires = tires
+
+class Car(GroundVehicle):
+    def __init__(self,name, coloe, tires, axel)
+    super().__init__(name, color, tires)
+    self.axel = axel
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name, color, tires, seats)
+    super().__init__(name, color, tires)
+    self.seats = seats
+
